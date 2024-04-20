01f6053ba711329b4492fe618a99db4358ec9237
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-19 22:38:03 -0600

    :recycle: Moved patch logic to ModManager

--- Main.cs
+++ Main.cs
@@ -1,34 +1,18 @@
 ﻿using MelonLoader;
 using SkipLogo.Managers;
-using SkipLogo.Patches;
 using SkipLogo.Properties;
 
 namespace SkipLogo;
 
 using static SettingsManager;
+using static ModManager;
 
 public class Main : MelonMod
 {
     public override void OnInitializeMelon()
     {
         Load();
-        PatchMethods();
+        PatchMethods(HarmonyInstance);
         LoggerInstance.Msg($"{MelonBuildInfo.ModName} has loaded correctly!");
-    }
-
-    private void PatchMethods()
-    {
-        if (!IsEnabled) return;
-
-        if (SkipToHome)
-        {
-            SkipToHomePatch.Patch(HarmonyInstance);
-            return;
-        }
-
-        if (SkipToWelcome)
-        {
-            SkipToWelcomePatch.Patch(HarmonyInstance);
-        }
     }
 }
---
+++ Managers/ModManager.cs
@@ -0,0 +1,24 @@
+﻿using SkipLogo.Patches;
+
+namespace SkipLogo.Managers;
+
+using static SettingsManager;
+
+internal static class ModManager
+{
+    internal static void PatchMethods(HarmonyLib.Harmony harmony)
+    {
+        if (!IsEnabled) return;
+
+        if (SkipToHome)
+        {
+            SkipToHomePatch.Patch(harmony);
+            return;
+        }
+
+        if (SkipToWelcome)
+        {
+            SkipToWelcomePatch.Patch(harmony);
+        }
+    }
+}
